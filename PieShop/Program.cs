cff0186264ee64cbd3055cf7c24e0f783f8307d7
anajmowicz <dorsha37@gmail.com>
2023-04-13 16:48:44 +0200

    Add project files.

---
+++ PieShop/Program.cs
@@ -0,0 +1,16 @@
+using PieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}   
+app.MapDefaultControllerRoute();
+app.Run();
