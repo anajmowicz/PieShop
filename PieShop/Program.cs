f9f7be6ca691a92bebb1c851944a384a636cf782
anajmowicz <dorsha37@gmail.com>
2023-04-19 21:07:36 +0200

    added Razor page as alternative checkout view

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCa
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<PieShopDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("PieShopDbContextConnection")));
 var app = builder.Build();
@@ -21,5 +23,6 @@ if(app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }   
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 DbInitializer.Seed(app);
 app.Run();
