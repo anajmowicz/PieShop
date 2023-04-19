b56d9e256730304b72b4c512d60da9e4d1348991
anajmowicz <dorsha37@gmail.com>
2023-04-19 22:43:23 +0200

    added unit tests for PieController and EmailTagHelper

---
+++ PieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Controllers;
+using PieShop.ViewModels;
+using PieShopTests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            //arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
